2bd9561a032e21780d616614eefe6f308df7d100
CheshtaK <32400008+CheshtaK@users.noreply.github.com>
2018-07-05 10:47:11 +0530

    final sentence translation for every case

--- elasticcode2.py
+++ elasticcode2.py
@@ -11,47 +11,53 @@ import pprint
 es = Elasticsearch(HOST = "http://localhost", PORT = 9200)
 es = Elasticsearch()
 
-with open('paytm.txt', encoding = 'utf-8') as f:
-    lineNum = 0
-    txtNum = 0
-    uNum = 0
-    for line in f:
-        lineNum += 1
-        print(lineNum)
-        if len(line) > 0:
-            if '|' in line:
-                txtNum += 1
-                val = re.split('[\t|]+', line)
-                
-                if val[2].find('Upto') != -1 and val[2].find('Cashback') != -1:
-                    print('Upto')
-                    uNum += 1
-                    es.index(index='upto', doc_type='file', id=uNum, body = {'Translation': val[0],'English': val[2]}, request_timeout=30)
-                    
-                es.index(index='conversion', doc_type='file', id=txtNum, body = {'Translation': val[0],'English': val[2]}, request_timeout=30)
-                
-                txtNum += 1
-                if val[3].find('Upto') != -1 and val[3].find('Cashback') != -1:
-                    print('Upto')
-                    uNum += 1
-                    es.index(index='upto', doc_type='file', id=uNum, body = {'Translation': val[1],'English': val[3]}, request_timeout=30)
-                    
-                es.index(index='conversion', doc_type='file', id=txtNum, body = {'Translation': val[1],'English': val[3]}, request_timeout=30)
-                
-            else:
-                txtNum += 1
-                val = line.split('\t')
-                if val[1].find('Upto') != -1 and val[1].find('Cashback') != -1:
-                    print('Upto')
-                    uNum += 1
-                    es.index(index='upto', doc_type='file', id=uNum, body = {'Translation': val[0],'English': val[1]}, request_timeout=30)
-
-                es.index(index='conversion', doc_type='file', id=txtNum, body = {'Translation': val[0],'English': val[1]}, request_timeout=30)
 
 
+'''Indexing the corpus'''
+##with open('paytm2.txt', encoding = 'utf-8') as f:
+##    lineNum = 0
+##    txtNum = 0
+##    uNum = 0
+##    for line in f:
+##        lineNum += 1
+##        print(lineNum)
+##        if len(line) > 0:
+##            if '|' in line:
+##                txtNum += 1
+##                val = re.split('[\t|]+', line)
+##                
+##                if val[2].find('Upto') != -1 and val[2].find('Cashback') != -1:
+##                    print('Upto')
+##                    uNum += 1
+##                    es.index(index='upto', doc_type='file', id=uNum, body = {'Translation': val[0],'English': val[2]}, request_timeout=30)
+##                    
+##                es.index(index='conversion', doc_type='file', id=txtNum, body = {'Translation': val[0],'English': val[2]}, request_timeout=30)
+##                
+##                txtNum += 1
+##                if val[3].find('Upto') != -1 and val[3].find('Cashback') != -1:
+##                    print('Upto')
+##                    uNum += 1
+##                    es.index(index='upto', doc_type='file', id=uNum, body = {'Translation': val[1],'English': val[3]}, request_timeout=30)
+##                    
+##                es.index(index='conversion', doc_type='file', id=txtNum, body = {'Translation': val[1],'English': val[3]}, request_timeout=30)
+##                
+##            else:
+##                txtNum += 1
+##                val = line.split('\t')
+##                if val[1].find('Upto') != -1 and val[1].find('Cashback') != -1:
+##                    print('Upto')
+##                    uNum += 1
+##                    es.index(index='upto', doc_type='file', id=uNum, body = {'Translation': val[0],'English': val[1]}, request_timeout=30)
+##
+##                es.index(index='conversion', doc_type='file', id=txtNum, body = {'Translation': val[0],'English': val[1]}, request_timeout=30)
 
+
+
+'''Check if indexed'''
 ##res = es.get(index = "conversion", doc_type = "file", id = "1")
 ##print(res['_source'])
+
+
 
 '''Function to translate a single word at a time'''
 def translate(lst):
@@ -84,24 +90,66 @@ def translate(lst):
     return t    
 
 
+'''Main Translation'''
 
-sentence = 'Upto Rs. 50 Cashback'
+sentence = 'supply chain management'
+sentence = sentence.lower()
 
-if sentence.find('Upto') != -1 and sentence.find('Cashback') != -1:
-    res = es.search(index='upto', body={'query': {'match' : { 'English' : sentence }}})
 
-    english = []
-    hindi = []
+res = es.search(index='conversion', body={'query': {'match' : { 'English' : sentence }}})
 
-    for hit in res['hits']['hits']:
+english = []
+temp = []
+hindi = []
+exists = False
+
+for hit in res['hits']['hits']:
+    print(hit['_id'], '->', hit['_source'])
+    temp.append(hit['_source']['English'].lower())
+    hindi.append(hit['_source']['Translation'])
+    print()
+
+print('HINDI')
+print(hindi,'\n')
+
+
+'''Cleaning the English corpus'''
+for line in temp:
+    english.append(line.replace('\n',''))
+
+print('ENGLISH')
+print(english,'\n')
+
+
+#If line exists in given corpus
+for line in english:
+    if line.lower() == sentence:
+        exists = True
+        for hit in res['hits']['hits']:
+            if hit['_source']['English'].lower() == sentence+'\n':
+                print(hit['_source']['Translation'])
+                break
+
+
+print(exists)
+
+
+#UPTO CASHBACK case
+if sentence.find('Upto') != -1 and sentence.find('Cashback') != -1 and exists == False:
+    resUC = es.search(index='upto', body={'query': {'match' : { 'English' : sentence }}})
+
+    ucEnglish = []
+    ucHindi = []
+
+    for hit in resUC['hits']['hits']:
         print(hit['_id'], '->', hit['_source'])
-        english.append(hit['_source']['English'])
-        hindi.append(hit['_source']['Translation'])
+        ucEnglish.append(hit['_source']['English'].lower())
+        ucHindi.append(hit['_source']['Translation'])
         
-    remove = list(set(english[0].split()) - set(sentence.split()))
+    remove = list(set(ucEnglish[0].split()) - set(sentence.split()))
     print('REMOVE', remove, '\n')
 
-    find = list(set(sentence.split()) - set(english[0].split()))
+    find = list(set(sentence.split()) - set(ucEnglish[0].split()))
     print('FIND', find, '\n')
 
     removeT = translate(remove)
@@ -110,87 +158,37 @@ if sentence.find('Upto') != -1 and sentence.find('Cashback') != -1:
     findT = translate(find)
     print('FIND TRANSLATED', findT)
 
-    hindi[0] = hindi[0].replace(remove[0], str(''.join(findT)))
+    ucHindi[0] = ucHindi[0].replace(removeT[0], str(''.join(findT)))
+    print(ucHindi[0])
+
+
+#When sentence length equal to search result
+elif len(sentence) == len(english[0]) and exists == False:
+    words = sentence.split()
+    t = translate(words)
+    print(str(' '.join(t)))
+
+
+#Length of search result greater than sentence
+elif len(sentence) < len(english[0]):
+    remove = list(set(english[0].split()) - set(sentence.split()))
+    t = translate(remove)
+
+    for i in range(len(t)):
+        hindi[0] = hindi[0].replace(t[i],'')
+
     print(hindi[0])
 
 
-##'''Main Translation'''
-###(UPTO CASHBACK cases) 
-##
-##sentence = 'Upto Rs. 50 Cashback'
-##
-##translation = 'तक का कैशबैक'
-##
-##res = es.search(index='conversion', body={'query': {'match' : { 'English' : sentence }}})
-##
-##english = []
-##nenglish = []
-##hindi = []
-##exists = False
-##
-##for hit in res['hits']['hits']:
-##    print(hit['_id'], '->', hit['_source'])
-##    english.append(hit['_source']['English'])
-##    hindi.append(hit['_source']['Translation'])
-##    print()
-##
-##print('HINDI')
-##print(hindi,'\n')
-##
-##
-##'''Cleaning the English corpus'''
-##for line in english:
-##    nenglish.append(line.replace('\n',''))
-##
-##print('ENGLISH')
-##print(nenglish,'\n')
-##
-##
-###UPTO CASHBACK
-##if sentence.find('Upto') != -1 and sentence.find('Cashback') != -1:
-##    start, *middle, end = sentence.split()
-##    if '%' not in sentence:
-##        number = [s for s in middle if s.isdigit()]
-##        toTranslate = [s for s in middle if not s.isdigit()]
-##        translated = translate(toTranslate)
-##        print('TRANSLATED', translated)
-##        print(translated[0]+' '+number[0]+' '+translation)
-##    else:
-##        print(str(''.join(middle))+' '+ translation)
-##
-##else:
-##    print('WHY AM I IN ELSE?')
-##    #If line exists in given corpus
-##    for line in nenglish:
-##        if line.lower() == sentence.lower():
-##            exists = True
-##            for hit in res['hits']['hits']:
-##                if hit['_source']['English'] == sentence+'\n':
-##                    print(hit['_source']['Translation'])
-##                    break
-##
-##    #When sentence length equal to search result
-##    if len(sentence) == len(nenglish[0]) and exists == False:
-##        words = sentence.split()
-##        t = translate(words)
-##        print(str(' '.join(t)))
-##
-##    #Length of search result greater than sentence
-##    elif len(sentence) < len(nenglish[0]):
-##        remove = list(set(nenglish[0].split()) - set(sentence.split()))
-##        t = translate(remove)
-##
-##        for i in range(len(t)):
-##            hindi[0] = hindi[0].replace(t[i],'')
-##
-##        print(hindi[0])
-##
-##
-##    ###When the sentence has only one word(for accurate results)
-##    ##if len(sentence.split()) == 1 and exists == False:
-##    ##    search = []
-##    ##    search.append(sentence)
-##    ##    print(str(''.join(translate(search))))
+
+
+
+
+###When the sentence has only one word(for accurate results)
+##if len(sentence.split()) == 1 and exists == False:
+##    search = []
+##    search.append(sentence)
+##    print(str(''.join(translate(search))))
 
 
 
